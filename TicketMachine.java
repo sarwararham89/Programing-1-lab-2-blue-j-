e598d2d4862d13f305dbf204be94d354830ab409
Arham Sarwar <sarwararham89@gmail.com>
2025-09-03 21:11:17 -0400

    made a declaration for the step number 17.

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,6 +17,7 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    private int status; 
 
     /**
      * Create a machine that issues tickets of the given price.
